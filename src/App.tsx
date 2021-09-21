0ced0abfa150113139742923f8ed25d7b2492f70
morozig <morozig@gmail.com>
2021-09-21 08:56:48 +0300

    browser router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './containers/home';
 import View from './containers/view';
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
